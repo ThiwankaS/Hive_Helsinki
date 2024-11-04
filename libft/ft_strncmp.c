b464ebc94cea48f0cdb5714aadc846330bf43774
Thiwanka Somachandra <tsomacha@c2r6p13.hive.fi>
2024-11-04 16:39:10 +0200

    Implementing strncmp and testing

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 16:24:55 by tsomacha          #+#    #+#             */
+/*   Updated: 2024/11/04 16:24:57 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_strncmp(const char *str1, const char *str2, unsigned int size)
+int	ft_strncmp(const char *str1, const char *str2, unsigned int size)
 {
-    unsigned int count;
+	unsigned int	count;
 
-    count = 0;
-    while ((*(str1 + count) || *(str2 + count)) && (count < size))
-    {
-        if (*(str1 + count) != *(str2 + count))
-        {
-            return (*(str1 + count) - *(str2 + count));
-        }
-        count++;
-    }
-    return (0);
+	count = 0;
+	while ((*(str1 + count) || *(str2 + count)) && (count < size))
+	{
+		if (*(str1 + count) != *(str2 + count))
+		{
+			return (*(str1 + count) - *(str2 + count));
+		}
+		count++;
+	}
+	return (0);
 }
