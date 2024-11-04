1dbc30063df343bb546c200b99f6974615da9149
Thiwanka Somachandra <tsomacha@c2r6p13.hive.fi>
2024-11-04 16:36:33 +0200

    Implementing strchr and testing

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,12 +1,24 @@
-const char *ft_strchr(const char *str, int param)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 16:22:27 by tsomacha          #+#    #+#             */
+/*   Updated: 2024/11/04 16:22:41 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+const char	*ft_strchr(const char *str, int param)
 {
-    while (*str)
-    {
-        if (*str == param)
-        {
-            return (str);
-        }
-        str++;
-    }
-    return ((char *)0);
+	while (*str)
+	{
+		if (*str == param)
+		{
+			return (str);
+		}
+		str++;
+	}
+	return ((char *)0);
 }
