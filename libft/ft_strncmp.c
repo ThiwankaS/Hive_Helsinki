6ece4813be64f50bab6646f078c3aeb56d36f99b
thiwankas <thivankas@outlook.com>
2024-11-04 06:26:57 +0200

    Implementing strncmp and testing : modification

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,9 +6,9 @@ int ft_strncmp(const char *str1, const char *str2, unsigned int size)
     count = 0;
     while ((*(str1 + count) || *(str2 + count)) && (count < size))
     {
-        if (str1[count] != str2[count])
+        if (*(str1 + count) != *(str2 + count))
         {
-            return (str1[count] - str2[count]);
+            return (*(str1 + count) - *(str2 + count));
         }
         count++;
     }
